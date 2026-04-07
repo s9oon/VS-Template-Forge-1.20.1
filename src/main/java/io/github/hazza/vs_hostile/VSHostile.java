dc570c7eae1cc739e96ae77c38f6f54218ba4a3c
Harry Barrett <harrybarrett178@gmail.com>
2026-04-08 09:41:10 +1200

    Changed names still template

---
+++ src/main/java/io/github/hazza/vs_hostile/VSHostile.java
@@ -0,0 +1,12 @@
+package io.github.hazza.vs_hostile;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(VSHostile.MOD_ID)
+public class VSHostile {
+    public static final String MOD_ID = "vs_hostile";
+
+    public VSHostile() {
+        // Common Code Here
+    }
+}
